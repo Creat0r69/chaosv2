1e1d7a893e1547df1c88174db2eef9c8339ba1ae
the creator <tcreat0r1337@gmail.com>
2024-09-26 21:26:56 +0100

    a

---
+++ script.js
@@ -0,0 +1,18 @@
+const song = new Audio('cool song.mp3');
+const container = document.getElementById('container')
+const vaxxed = document.getElementById('vaxxed')
+function access(parameter){
+    if (parameter == 'granted'){
+        vaxxed.style.visibility = "hidden";
+        container.style.visibility = "visible";
+        song.play()
+        song.loop = true;
+    }
+    else{
+        alert('buy 💉🧬 and come back later!')
+        setTimeout(function(){window.location.href = "https://www.unicorn.meme/";}, 1000)
+    }
+}
+function lore(){
+    window.open('https://vaxx-marketvaccinesslegal.online/lore');
+}
